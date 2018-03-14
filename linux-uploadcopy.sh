d7ac923629057742774473aa7278152d3d7929d0
Devilsking <leonguyen52@outlook.com>
2018-03-14 13:30:17 +0800

    Create linux-uploadcopy.sh

---
+++ linux-uploadcopy.sh
@@ -0,0 +1,20 @@
+#Google Drive Upload Tools to Cloud
+
+#!/bin/bash
+
+UPLOAD_NAME=UploadSeeding
+
+TIMESTAMP=$(date +"%F")
+BACKUP_DIR="/root/Seeding"
+SECONDS=0
+
+size=$(du -sh $BACKUP_DIR | awk '{ print $1}')
+
+echo "Start uploading seeding folder";
+echo "This folder will remain on VPS after uploading";
+/usr/sbin/rclone copy $BACKUP_DIR "gdrive:$UPLOAD_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+echo "Finished";
+echo '';
+
+duration=$SECONDS
+echo "Total $size, $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
