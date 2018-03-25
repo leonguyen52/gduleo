01429e3059725d6760b21a7ea89d8c7578cf87ff
Devilsking <leonguyen52@outlook.com>
2018-03-25 18:57:38 +0800

    Update linux-uploadcopy.sh

--- linux-uploadcopy.sh
+++ linux-uploadcopy.sh
@@ -12,7 +12,7 @@ size=$(du -sh $BACKUP_DIR | awk '{ print $1}')
 
 echo "Start uploading seeding folder";
 echo "This folder will remain on VPS after uploading";
-/usr/sbin/rclone copy $BACKUP_DIR "gdrive:$UPLOAD_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+/usr/sbin/rclone copy $BACKUP_DIR "gdrive:$UPLOAD_NAME/$TIMESTAMP" --stats=10s -v
 echo "Finished";
 echo '';
 
