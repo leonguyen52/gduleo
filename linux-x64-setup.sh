393e75edd3e8a9e28b812ef41c2a864c10b917c4
Devilsking <leonguyen52@outlook.com>
2018-06-19 08:52:06 +0800

    Update linux-x64-setup.sh

--- linux-x64-setup.sh
+++ linux-x64-setup.sh
@@ -3,4 +3,5 @@ wget https://downloads.rclone.org/v1.41/rclone-v1.41-linux-amd64.zip
 unzip rclone-v*.zip
 \cp rclone-v*-linux-amd64/rclone /usr/sbin/
 rm -rf rclone-*
+mkdir /root/Downloads
 rclone config
